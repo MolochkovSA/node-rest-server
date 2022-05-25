fa6735fd401fccfe932be1aa5684bf39e49ef237
wwWSerGWww <wwwsergwww@mail.ru>
2022-05-25 12:52:02 +0300

    refactoring

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 const app = express()
 const port = 8080
+
 //NRS-3: Request time by performance.now
 app.use((req, res, next) => {
   req.start = performance.now()
@@ -10,39 +11,6 @@ app.use((req, res, next) => {
       `Request time: ${(performance.now() - req.start).toFixed(3)} ms`
     )
   })
-  next()
-})
-
-//NRS-3: Request time by performance.mark
-// app.use((req, res, next) => {
-//   performance.mark('start')
-//   res.on('finish', () => {
-//     performance.mark('end')
-//     performance.measure('perf', 'start', 'end')
-//     console.log(
-//       `Request time: ${performance
-//         .getEntriesByName('perf')
-//         .pop()
-//         .duration.toFixed(3)} ms`
-//     )
-//   })
-//   next()
-// })
-
-//NRS-2: log request/respons
-app.use((req, res, next) => {
-  console.log(`
-Request:
-  Method: ${req.method}
-  URL: ${req.url}
-  Headers: ${JSON.stringify(req.headers)}
-  Query: ${JSON.stringify(req.query)}
-  Body:  ${req.body || 'не указано'}
-Respons:
-  Code: ${res.statusCode}
-  Headers: ${JSON.stringify(res.getHeaders())}
-  Body: ${res.body || 'не указано'}`)
-  // console.log(http.ServerResponse)
   next()
 })
 
