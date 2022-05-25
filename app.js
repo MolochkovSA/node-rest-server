63fa4143a0ade793f4eba0e9b4fd9df405fde0a3
wwWSerGWww <wwwsergwww@mail.ru>
2022-05-25 12:12:01 +0300

    NRS-2: Log request/respons

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ import express from 'express'
 const app = express()
 const port = 8080
 
+//NRS-2: Log request/respons
+let resBody
+app.use(function responseLogger(req, res, next) {
+  const originalSendFunc = res.send.bind(res)
+  res.send = function (body) {
+    resBody = body
+    return originalSendFunc(body)
+  }
+  next()
+})
+app.use((req, res, next) => {
+  console.log(
+    `Request:
+  Method: ${req.method}
+  URL: ${req.url}
+  Headers: ${JSON.stringify(req.headers)}
+  Query: ${JSON.stringify(req.query)}
+  Body:  ${JSON.stringify(req.body)}
+Respons:
+  Code: ${res.statusCode}
+  Headers: ${JSON.stringify(res.getHeaders())}
+  Body: ${resBody}`
+  )
+  next()
+})
+
 //NRS-1: Implement hello world API
 app.get('/api/hello', (req, res) => {
   res.send('Hello World')
